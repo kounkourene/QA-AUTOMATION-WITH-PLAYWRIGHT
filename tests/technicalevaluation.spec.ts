72c9b1405824505464ac7baf588121bd412b1aa2
kounkourene <kounkourene@gmail.com>
2025-02-04 11:27:46 -0700

    Implemented Mobile Application Suite

--- tests/technicalevaluation.spec.ts
+++ tests/technicalevaluation.spec.ts
@@ -14,7 +14,7 @@ test.describe("Web Application", () => {
     //Making sure we are in the To Do column
     await page.getByText("To Do").click();
   });
-  test.skip("Test Case 1", async ({ page }) => {
+  test("Test Case 1", async ({ page }) => {
     //Verify "Implement user authentication" is in the "To Do" column.
     const taskisInToDoColumn = page.getByText("Implement user authentication");
     await expect(taskisInToDoColumn).toBeVisible();
@@ -47,7 +47,7 @@ test.describe("Web Application", () => {
       .getByText("Bug");
     await expect(tagBug).toContainText("Bug");
   });
-  test.skip("Test Case 3", async ({ page }) => {
+  test("Test Case 3", async ({ page }) => {
     //Making sure we are in the In Progress column
     await page.getByText("In Progress").click();
 
@@ -62,5 +62,60 @@ test.describe("Web Application", () => {
       .locator(".px-2")
       .getByText("Design");
     await expect(tagDesign).toContainText("Design");
+  });
+});
+
+test.describe("Mobile Application", () => {
+  test.beforeEach("Navigate to Mobile Application", async ({ page }) => {
+    await page.getByText("Mobile Application").click();
+  });
+
+  test("Test Case 4", async ({ page }) => {
+    //Making sure we are in To Do Culumn
+    await page.getByText("To Do").click();
+    //Verify "Push notification system" is in the "To Do" column.
+    const verifyPushNotification = page.getByText("Push notification system");
+    await expect(verifyPushNotification).toContainText(
+      "Push notification system"
+    );
+    //Confirm tags: "Feature”
+    const tagFeatureTestCase4 = page
+      .locator(".bg-white")
+      .filter({ hasText: "Push notification system" })
+      .getByText("Feature");
+    await expect(tagFeatureTestCase4).toContainText("Feature");
+  });
+  test("Test Case 5", async ({ page }) => {
+    //Making sure we are in the "In Progress" column
+    await page.getByText("In Progress").click();
+    //Verify "Offline mode" is in the "In Progress" column.
+    const verifyOflineMode = page.getByText("Offline mode");
+    await expect(verifyOflineMode).toBeVisible();
+    //Confirm tags: "Feature"
+    const confirmTagFeature = page
+      .locator(".bg-white")
+      .filter({ hasText: "Offline mode" })
+      .getByText("Feature");
+    await expect(confirmTagFeature).toContainText("Feature");
+    //Confirm tags: "High Priority"
+    const confirmTagHighPriority = page
+      .locator(".bg-white")
+      .filter({ hasText: "Offline mode" })
+      .getByText("High Priority");
+    await expect(confirmTagHighPriority).toContainText("High Priority");
+  });
+  test("Test Case 6", async ({ page }) => {
+    //Making sure we are in "Done" colum
+    await page.getByText("Done").click();
+    //Verify "App icon design" is in the "Done" column.
+    const verifyAppActionDesign = page.getByText("App icon design");
+    await expect(verifyAppActionDesign).toBeVisible();
+    //Confirm tags: "Design”
+    const confirmDesignTag = page
+      .locator(".bg-white")
+      .filter({ hasText: "App icon design" })
+      .locator(".px-2")
+      .getByText("Design");
+    await expect(confirmDesignTag).toContainText("Design");
   });
 });
